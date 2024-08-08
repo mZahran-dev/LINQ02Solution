7f45c88ca9973fed72c9f259504b4c193933b92d
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-08 17:12:36 +0300

    feat : Solve Q2 in LINQ - Element Operators

--- LINQ02/Program.cs
+++ LINQ02/Program.cs
@@ -8,10 +8,14 @@ namespace LINQ02
             #region LINQ - Element Operators
 
             #region 1.Get first Product out of Stock 
-            var Result = ProductList.FirstOrDefault(p=> p.UnitsInStock == 0);
-            Console.WriteLine(Result);
+            //var Result = ProductList.FirstOrDefault(p=> p.UnitsInStock == 0);
+            //Console.WriteLine(Result);
             #endregion
 
+            #region 2.Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+            var result = ProductList.FirstOrDefault(P => P.UnitPrice > 1000);
+            Console.WriteLine(result);
+            #endregion
             #endregion
         }
     }
