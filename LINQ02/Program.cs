91ab70678ed86c1a23173a06d8778a5fb504b90c
Mahmoud Zahran <mi4654110@gmail.com>
2024-08-08 17:06:13 +0300

    feat: Solve Q1 in LINQ - Element Operators

--- LINQ02/Program.cs
+++ LINQ02/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace LINQ02
+﻿using static LINQ02.ListGenerators;
+namespace LINQ02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LINQ - Element Operators
+
+            #region 1.Get first Product out of Stock 
+            var Result = ProductList.FirstOrDefault(p=> p.UnitsInStock == 0);
+            Console.WriteLine(Result);
+            #endregion
+
+            #endregion
         }
     }
 }
